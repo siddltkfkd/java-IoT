faa7afee0428c77785944c1108354b40d312976d
siddltkfkd <unji2000@gmail.com>
2023-09-08 10:30:21 +0900

    2진수를 10진수로

---
+++ 0906/src/main/java/Main.java
@@ -0,0 +1,52 @@
+public class Main {
+
+    private static final String[] TEST_CASE_KEYS = {
+            "00000001",
+            "101",
+            "1011",
+            "111000",
+            "1011111",
+            "1111111111111",
+            "00111001110001111100001"
+    };
+
+    private static final int[] TEST_CASE_VALUES = {
+            1,
+            5,
+            11,
+            56,
+            95,
+            8191,
+            1893345
+    };
+
+    private static double correct = 0;
+
+    public static void main(String[] args) {
+        for (int i = 0; i < TEST_CASE_KEYS.length; i++) {
+            System.out.println("Test Case " + (i + 1) + " = " + test(TEST_CASE_KEYS[i], TEST_CASE_VALUES[i]));
+        }
+
+        System.out.printf("정답률 = %.3f%%", (correct / TEST_CASE_KEYS.length * 100));
+    }
+
+    private static boolean test(String input, Integer result) {
+        if (solution(input) == result) {
+            correct++;
+            return true;
+        }
+
+        return false;
+    }
+
+    private static int solution(String input) {
+        String[] value = input.split("");
+        
+        int answer = Integer.parseInt(value[0]);
+        for (int i=0; i<value.length-1; i++) {
+            answer = answer * 2 + Integer.parseInt(value[i+1]);
+        }
+        // System.out.println(answer);
+        return answer;
+    }
+}
